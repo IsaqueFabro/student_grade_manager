b1ebabc763403258e423e5cfe6aebecdce37bd9a
“Isaque <isaquefabro04@gmail.com>
2025-07-04 21:37:53 -0300

    api running with /grades

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-import http from 'http'
+import { write } from 'fs';
+import http, { STATUS_CODES } from 'http'
 
 const port = 3000
+const grades = [
+    {
+        studantName: "Isaque",
+        subject: "Mathematics",
+        grade: "9.2"
+    }
+]
 
 const server = http.createServer((request, response) => {
-    // funcoes do back
-})
+    const { method, url } = request;
 
+    if (url === '/grades' && method === 'GET') {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(grades));
+    }else {
+        response.writeHead(404, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify({ message: 'Not Found' }));
+    }
+});
 
 server.listen(port, () => {
     console.log(`Server is running on ${port}`);
-})
+});
